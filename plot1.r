12ce7aab95a5e2252778f4a4e792e4fe9b10873a
Tanmoy Rath <tanmoy22rath@gmail.com>
2019-03-21 21:20:07 +0530

    Exploratory Analysis - Project 1

--- plot1.r
+++ plot1.r
@@ -7,5 +7,5 @@ final <- readr::type_convert(final)
 final$date_time <- strptime(  paste(final$Date,final$Time),  format = "%d/%m/%Y %H:%M:%S")
 
 png(filename = "plot1.png", width=480, height=480)
-hist(final$Global_active_power, col="red", main="Histogram of Global Active Power", xlab = "Global Active Power (kilowatts)")
+hist(final$Global_active_power, col="red", main="Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
